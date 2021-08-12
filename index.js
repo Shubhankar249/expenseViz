c3ade831ddfa603ed31cf733a6968d1d8e78cc9c
Shubhankar <shubhankar.andi5@gmail.com>
2021-08-12 10:35:10 +0530

    deploy2

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.post('/create-room', RoomController.Create);
 app.get('/join-room/', RoomController.Join);
 app.post('/transaction', RoomController.AddTransaction);
 
-app.listen(port || process.env.PORT, (err) => {
+app.listen(process.env.PORT || port , (err) => {
     if (err) console.log(`Error starting the server ${err}`);
     console.log(`Server is running on localhost:${port}`);
 })
